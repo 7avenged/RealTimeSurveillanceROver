0908cd9cc422f91cd497d126ab51012fe62ccdec
7avenged <7avenged@users.noreply.github.com>
2017-09-11 22:40:11 +0530

    Add files via upload

---
+++ l293_bluetooth.ino
@@ -0,0 +1,101 @@
+   
+  const int motorA1  = 5;  
+  const int motorA2  = 6;  
+  const int motorB1  = 10; 
+  const int motorB2  = 9;  
+ 
+  const int bluein = 2;
+  int perVolt;                 
+  float voltage = 0.0;         
+  int level;
+  int i=0;
+  int j=0;
+  int state;
+  int vSpeed=200;    
+
+void setup() {
+    pinMode(motorA1, OUTPUT);
+    pinMode(motorA2, OUTPUT);
+    pinMode(motorB1, OUTPUT);
+    pinMode(motorB2, OUTPUT);
+   
+    pinMode(bluein, INPUT);     
+    Serial.begin(9600);
+}
+ 
+void loop() {
+     if(digitalRead(bluein)==LOW) { state='S'; 
+    }
+
+    if(Serial.available() > 0){     
+      state = Serial.read();   
+    }
+    if (state == '0'){
+      vSpeed=0;}
+    else if (state == '1'){
+      vSpeed=100;}
+    else if (state == '2'){
+      vSpeed=180;}
+    else if (state == '3'){
+      vSpeed=200;}
+    else if (state == '4'){
+      vSpeed=255;}
+    if (state == 'F') {
+    	analogWrite(motorA1, vSpeed); 
+	analogWrite(motorA2, 0);
+        analogWrite(motorB1, 0);      
+	analogWrite(motorB2, 0); 
+    }
+    else if (state == 'G') {
+    	analogWrite(motorA1, vSpeed);
+	analogWrite(motorA2, 0);  
+        analogWrite(motorB1, 200);  
+        analogWrite(motorB2, 0); 
+    } 
+    else if (state == 'I') {
+      	analogWrite(motorA1, vSpeed); 
+	analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 0);      
+	analogWrite(motorB2, 200); 
+    }   
+    else if (state == 'H') {
+    	analogWrite(motorA1, 0);
+	   analogWrite(motorA2, vSpeed); 
+        analogWrite(motorB1, 200); 
+	analogWrite(motorB2, 0); 
+    }
+  
+    else if (state == 'J') {
+    	analogWrite(motorA1, 0); 
+  analogWrite(motorA2, vSpeed); 
+        analogWrite(motorB1, 0);  
+ analogWrite(motorB2, 200); 
+    }
+    else if (state == 'L') {
+    	analogWrite(motorA1, 0);  
+ 	analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 200); 
+	analogWrite(motorB2, 0); 
+    }
+ 
+    else if (state == 'R') {
+    	analogWrite(motorA1, 0);   
+	analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 0);   
+	analogWrite(motorB2, 200); 		
+    }
+      state='n';  
+    }
+    else if (state == 'S'){
+        analogWrite(motorA1, 0);  
+	analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 0);  
+	analogWrite(motorB2, 0);
+    }
+  
+    
+}
+
+
+
+
